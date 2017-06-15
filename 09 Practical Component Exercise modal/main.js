0f304c09f50d81878462eec9a06b45e2455a36c7
guoyaqi <1664216915@qq.com>
2017-06-15 17:25:08 +0800

    09 Practical Component Exercise modal

---
+++ 09 Practical Component Exercise modal/main.js
@@ -0,0 +1,32 @@
+Vue.component('modal', {
+  template: `
+    <div class="modal" :class="{'is-active': isActive}">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">123</div>
+        </div>
+        <button class="modal-close" v-on:click="hideModal"></button>
+    </div>
+  `,
+  props: ['isActive'],
+  methods: {
+    hideModal: function () {
+      this.$emit('hide-modal');
+    }
+  }
+})
+
+new Vue({
+  el: '#app',
+  data: {
+    isActive: false
+  },
+  methods: {
+    showModal: function () {
+      this.isActive = true;
+    },
+    hideModal: function () {
+      this.isActive = false;
+    }
+  }
+})
